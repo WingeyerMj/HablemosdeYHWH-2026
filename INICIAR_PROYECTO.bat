8d88d0cc4b66a598d02ba97d377843a83a8cff7b
Marcelo <wingeyer.mj@gmail.com>
2026-05-12 00:08:29 -0300

    arreglando el proyecto

---
+++ INICIAR_PROYECTO.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Iniciando servidor de Hablemos de YHWH en el puerto 3001...
+cd node-backend
+npm install && node app.js
+pause
--- node-backend/app.js
+++ node-backend/app.js
@@ -25,6 +25,7 @@ app.set('views', path.join(__dirname, 'src/views'));
 app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/calendar', express.static(path.join(__dirname, '../Calendar')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
