0abf5b251bc59a7a9cd3d51b9c48ded28efb76f7
Marcelo <wingeyer.mj@gmail.com>
2026-05-13 20:36:09 -0300

    arreglo

--- hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'admin123'; // Cambia esto por la contraseña que quieras
-bcrypt.hash(password, 10, (err, hash) => {
-    if (err) throw err;
-    console.log('Tu contraseña hasheada es:');
-    console.log(hash);
-    console.log('\nCopia este hash y pégalo en la base de datos (tabla users).');
-});
