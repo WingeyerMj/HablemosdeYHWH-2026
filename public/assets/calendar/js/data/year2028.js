98a47319cfad7800e0ae291e8d7c972d5712b2d1
Marcelo <wingeyer.mj@gmail.com>
2026-08-16 15:12:56 -0300

    Revert "fix(calendar): aplicar regla de 2 días de luna oscura (se toma el segundo día y el mes inicia al día siguiente)"
    This reverts commit 70e7c0dc89e63a9655e25177dca485edde6f2aa7.

--- public/assets/calendar/js/data/year2028.js
+++ public/assets/calendar/js/data/year2028.js
@@ -2,80 +2,80 @@
 
 export const lunisolar2028 = {
   year: 2028,
-  roshHashana: "2028-03-26T18:00:00Z",
+  roshHashana: "2028-03-25T18:00:00Z", // Rosh Hashaná con 0% de luminosidad
   hasLeapMonth: false,
   months: [
     {
       index: 0,
       name: "Abib",
-      roshJodes: "2028-03-26T18:00:00Z",
-      next: "2028-04-25T18:00:00Z"
+      roshJodes: "2028-03-25T18:00:00Z",
+      next: "2028-04-24T18:00:00Z"
     },
     {
       index: 1,
       name: "Mes 2",
-      roshJodes: "2028-04-25T18:00:00Z",
-      next: "2028-05-24T18:00:00Z"
+      roshJodes: "2028-04-24T18:00:00Z",
+      next: "2028-05-23T18:00:00Z"
     },
     {
       index: 2,
       name: "Mes 3",
-      roshJodes: "2028-05-24T18:00:00Z",
-      next: "2028-06-23T18:00:00Z"
+      roshJodes: "2028-05-23T18:00:00Z",
+      next: "2028-06-22T18:00:00Z"
     },
     {
       index: 3,
       name: "Mes 4",
-      roshJodes: "2028-06-23T18:00:00Z",
-      next: "2028-07-22T18:00:00Z"
+      roshJodes: "2028-06-22T18:00:00Z",
+      next: "2028-07-21T18:00:00Z"
     },
     {
       index: 4,
       name: "Mes 5",
-      roshJodes: "2028-07-22T18:00:00Z",
-      next: "2028-08-21T18:00:00Z"
+      roshJodes: "2028-07-21T18:00:00Z",
+      next: "2028-08-20T18:00:00Z"
     },
     {
       index: 5,
       name: "Mes 6",
-      roshJodes: "2028-08-21T18:00:00Z",
-      next: "2028-09-20T18:00:00Z"
+      roshJodes: "2028-08-20T18:00:00Z",
+      next: "2028-09-19T18:00:00Z"
     },
     {
       index: 6,
       name: "Mes 7",
-      roshJodes: "2028-09-20T18:00:00Z",
-      next: "2028-10-19T18:00:00Z"
+      roshJodes: "2028-09-19T18:00:00Z",
+      next: "2028-10-18T18:00:00Z"
     },
     {
       index: 7,
       name: "Mes 8",
-      roshJodes: "2028-10-19T18:00:00Z",
-      next: "2028-11-18T18:00:00Z"
+      roshJodes: "2028-10-18T18:00:00Z",
+      next: "2028-11-17T18:00:00Z"
     },
     {
       index: 8,
       name: "Mes 9",
-      roshJodes: "2028-11-18T18:00:00Z",
-      next: "2028-12-17T18:00:00Z"
+      roshJodes: "2028-11-17T18:00:00Z",
+      next: "2028-12-16T18:00:00Z"
     },
     {
       index: 9,
       name: "Mes 10",
-      roshJodes: "2028-12-17T18:00:00Z",
-      next: "2029-01-16T18:00:00Z"
+      roshJodes: "2028-12-16T18:00:00Z",
+      next: "2029-01-15T18:00:00Z"
     },
     {
       index: 10,
       name: "Mes 11",
-      roshJodes: "2029-01-16T18:00:00Z",
-      next: "2029-02-14T18:00:00Z"
+      roshJodes: "2029-01-15T18:00:00Z",
+      next: "2029-02-13T18:00:00Z"
     },
     {
       index: 11,
       name: "Mes 12",
-      roshJodes: "2029-02-14T18:00:00Z",
-      next: "2029-03-16T18:00:00Z"
+      roshJodes: "2029-02-13T18:00:00Z",
+      next: "2029-03-14T18:00:00Z"
     }
   ]
 };
